b135dd35e13cc2ce75f79a55d3c21ca0c6652186
Advait Sumiran <117119063+AdvaitSumiran@users.noreply.github.com>
2023-09-21 13:06:56 +0000

    DateAndTime.js

---
+++ 01_basics/Date1.js
@@ -0,0 +1,21 @@
+let myDate = new Date(2023,0,12)
+
+//console.log(myDate.toDateString)
+
+let newDate1 = Date.now()
+console.log(myDate.getTime())
+
+console.log(Date.now()/1000)
+
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+
+console.log(newDate.getMonth())
+
+console.log(newDate.getFullYear())
+
+console.log(newDate.getMinutes())
+
+console.log(newDate.getDay())
