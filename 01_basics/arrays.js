c54d1fcec86cdc753a75f1d057c6762bde0de1e3
Advait Sumiran <117119063+AdvaitSumiran@users.noreply.github.com>
2023-09-21 14:27:09 +0000

    BasicsOfArray.js

---
+++ 01_basics/arrays.js
@@ -0,0 +1,44 @@
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[0])
+
+const myHeroes = ["Skatiman","Thor"]
+console.log(myHeroes)
+
+const  myHSingers= new Array("Honey Singh","Paradoxx")
+console.log(myHSingers)
+
+
+// Array Methods
+
+myArr.push(6)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+
+// myArr.unshift(9)
+// console.log(myArr)
+
+//console.log(myArr.includes(9))
+//console.log(myArr.indexOf(3))
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+
+// console.log(myArr)
+
+// *************Slice and Splice******************
+myArr.pop()
+
+const my1 = myArr.slice(1,3)
+console.log(my1)
+console.log("B",myArr)
+
+const my2 = myArr.splice(1,3)
+console.log(my2)
+console.log("c",myArr)
+
+const marvel_heroes = ["Ironman","Thor","Spiderman"]
+const dc_heroes = ["Superman","flash","batman"]
+
