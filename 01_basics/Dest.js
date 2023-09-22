5e60524a37b8cab308bc20a68402f100df60b3ab
Advait Sumiran <117119063+AdvaitSumiran@users.noreply.github.com>
2023-09-22 18:37:04 +0000

    ObjectDest.js

---
+++ 01_basics/Dest.js
@@ -0,0 +1,34 @@
+
+const course = {
+    courseName:"jsinhindi",
+    fee:"999",
+    courseInstructor:"HiteshSir",
+    courseId : "123abc"
+ }
+
+
+ const {courseInstructor} = course
+ console.log(courseInstructor)
+
+ const{fee} = course
+ console.log(fee)
+
+ const {courseName : Name} = course
+ console.log(Name)
+
+ const {courseId} = course
+ console.log(courseId)
+
+ // API
+// whenevere we are trying to make others responsible for our tasks then we use API
+
+
+// json => js object notation
+
+// The struct of an API looks more or less like a Object and sonmetimes as an array of object 
+
+// we will study about the API in detail in coming times
+
+// till then stay connected
+
+
