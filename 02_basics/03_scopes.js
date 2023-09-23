9fc958ed2f0ced673c46ee85e26261d3aa076a2a
Advait Sumiran <117119063+AdvaitSumiran@users.noreply.github.com>
2023-09-23 09:50:10 +0000

    ScopesInJs.js

---
+++ 02_basics/03_scopes.js
@@ -0,0 +1,38 @@
+function addone(num){
+    return num+1
+}
+
+addone(50)
+
+const addTwo = function(num){
+    return num+2
+}
+
+ addTwo(5)
+
+
+ function addThree(num){
+    return num+10
+ }
+ 
+addThree(10)
+
+const addThree = function(num){
+    return num+11
+}
+
+ //  There is a big difference here as here in the first type we are just declaring the function so we can call it even without decl it
+
+ // eg
+
+//  addone(5)
+//  function addone(num){
+//     return num+1
+// }
+
+// addTwo(7)
+// const addTwo = function(num){
+//     return num+2
+// }
+
+// The second one will give an error
